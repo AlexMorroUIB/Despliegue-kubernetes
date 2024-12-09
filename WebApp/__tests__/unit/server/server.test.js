ff090aa28331b1b25c65917c8b92410b0a867457
AlexMorroUIB <alexandre-joan.morro1@estudiant.uib.cat>
2024-12-09 01:49:55 +0100

    fix jest test

--- WebApp/__tests__/unit/server/server.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('../../../test.js')
-
-test('server returns code 200', () => {
-  expect(sum(2, 3)).toBe(5)
-})
---
+++ WebApp/__tests__/unit/server/tester.test.js
@@ -0,0 +1,5 @@
+const sum = require('../../../tester.js')
+
+test('sum 2 numbers', () => {
+  expect(sum(2, 3)).toBe(5)
+})
